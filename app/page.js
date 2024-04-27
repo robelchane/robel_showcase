f697d239ec4e4620cf29464554cc4765ee8d14ad
robelchane <robel.chane@edu.sait.ca>
2024-04-27 01:26:49 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function page(){
     <div className="flex flex-col justify-center items-center h-screen font-serif text-3xl">
       <h1 className="mb-5 text-3xl font-bold">Welcome to Robel Portfolio </h1>
       <p className="hover:text-gray-500 hover:underline mb-2"><Link href="final-project">Visit Robel Portfolio</Link></p>
-      <p className="hover:text-gray-500 hover:underline"><Link href="final-project-extra">Final Project Extra</Link></p>
+      <p className="hover:text-gray-500 hover:underline"><Link href="final-project-extra">Project Extra</Link></p>
     </div>
 
     </main>  
